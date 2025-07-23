cbe5c42704bdd6421df604e31c152e5fee512f3a
mridhops <mridhophs@gmail.com>
2025-07-23 07:10:11 +0700

    first change

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Start your journey here.
           </li>
         </ol>
 
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our documentt
           </a>
         </div>
       </main>
