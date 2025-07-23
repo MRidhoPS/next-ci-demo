2fc13c724b4905ac8ad428eb35368331f733b2f4
mridhops <mridhophs@gmail.com>
2025-07-23 08:04:40 +0700

    change workflows 4

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to javanice.com →
+          Go to zumbajakarta.com →
         </a>
       </footer>
     </div>
